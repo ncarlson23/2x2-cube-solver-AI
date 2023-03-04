9e65271f6f4f48d39852564c3e8a0802244c6425
Emma Shneidman <emmars@colostate.edu>
2023-03-04 12:37:47 +0000

    faces

--- CubeAI-main/RubiksCube.py
+++ CubeAI-main/RubiksCube.py
@@ -12,5 +12,12 @@ class RubiksCube():
     White = 'W'
     white_prime = 'W\''
 
+    face_one = 1
+    face_two = 2
+    face_three = 3
+    face_four = 4
+    face_five = 5
+    face_six = 6
+
     
 
