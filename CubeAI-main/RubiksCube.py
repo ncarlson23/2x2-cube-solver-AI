24733807fd6ec8f8d803320c96bbb193583f527e
Emma Shneidman <emmars@colostate.edu>
2023-03-04 12:35:07 +0000

    Color variables

--- CubeAI-main/RubiksCube.py
+++ CubeAI-main/RubiksCube.py
@@ -1,5 +1,16 @@
-# Testing git linking
+class RubiksCube():
+    Red = 'R'
+    red_prime = 'R\''
+    Orange = 'O'
+    orange_prime = 'O\''
+    Yellow = 'Y'
+    yellow_prime = 'Y\''
+    Green = 'G'
+    green_prime = 'G\''
+    Blue = 'B'
+    blue_prime = 'B\''
+    White = 'W'
+    white_prime = 'W\''
 
-#testing
+    
 
-# nina comment 
