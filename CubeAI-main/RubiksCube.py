f33dd2c6400fc7a66012e1a03b40e07cc292ccaf
Emma Shneidman <emmars@colostate.edu>
2023-03-04 13:01:03 +0000

    variable changes

--- CubeAI-main/RubiksCube.py
+++ CubeAI-main/RubiksCube.py
@@ -1,17 +1,13 @@
 class RubiksCube():
+    # variables to keep track of the colors
     Red = 'R'
-    red_prime = 'R\''
     Orange = 'O'
-    orange_prime = 'O\''
     Yellow = 'Y'
-    yellow_prime = 'Y\''
     Green = 'G'
-    green_prime = 'G\''
     Blue = 'B'
-    blue_prime = 'B\''
     White = 'W'
-    white_prime = 'W\''
 
+    # 
     face_one = 1
     face_two = 2
     face_three = 3
@@ -19,5 +15,5 @@ class RubiksCube():
     face_five = 5
     face_six = 6
 
-    
+
 
