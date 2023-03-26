64a01988a7472e5c5fee585ea9548cab7166e2ca
Nina Carlson <79552352+ncarlson23@users.noreply.github.com>
2023-03-26 18:50:13 +0000

    no dls

--- RubiksCube.py
+++ RubiksCube.py
@@ -306,28 +306,28 @@ class Cube:
                 return cube.moves, totalNodes, time() - start, depth
             depth += 1
 
-    def dls(self, cube0, limit):
-        nodeCount = 0
-        if cube0.isSolved() or limit <= 0:
-            return cube0, nodeCount
-        for move in MOVES.keys():
-            # Skips moves that lead to unnecessary states
-            if not cube0.validMove(move):
-                continue
-            # Applies move
-            state = self.applyMove(cube0.state, move)
-            cube = Cube(state, cube0.moves[:])
-            # Tracks the moves up to this cube
-            cube.addMove(move)
-            nodeCount += 1
-            # Recursively call depth limited search until limit is 0
-            cube, n = self.dls(cube, limit - 1)
-            nodeCount += n
-            if cube.isSolved():
-                return cube, nodeCount
-        return cube0,nodeCount
+#     def dls(self, cube0, limit):
+#         nodeCount = 0
+#         if cube0.isSolved() or limit <= 0:
+#             return cube0, nodeCount
+#         for move in MOVES.keys():
+#             # Skips moves that lead to unnecessary states
+#             if not cube0.validMove(move):
+#                 continue
+#             # Applies move
+#             state = self.applyMove(cube0.state, move)
+#             cube = Cube(state, cube0.moves[:])
+#             # Tracks the moves up to this cube
+#             cube.addMove(move)
+#             nodeCount += 1
+#             # Recursively call depth limited search until limit is 0
+#             cube, n = self.dls(cube, limit - 1)
+#             nodeCount += n
+#             if cube.isSolved():
+#                 return cube, nodeCount
+#         return cube0,nodeCount
 
-    # Solves the cube with breadth-first search
+    # Solves the cube with astar search
     def astar(self):
         if self.isSolved():
             return self.moves, 0, 0.0
